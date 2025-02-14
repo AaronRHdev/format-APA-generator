59ff8739c7734ac679446da2f9e01eb2e36ceac1
Aaron Ramos <aaron.rm.hr@gmail.com>
2025-02-14 15:38:28 -0600

    Modify the rute to add copy to clipboard

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -304,7 +304,7 @@ function copyToClipboard(){
 function createButtonCopy() {
     let btnCopy = document.createElement('img');
     btnCopy.setAttribute('onclick', 'copyToClipboard();');
-    btnCopy.src = '/assets/img/icons/copy-light.svg';
+    btnCopy.src = '../../assets/img/icons/copy-light.svg';
     btnCopy.classList.add('button-copy');
     btnCopy.id = 'copy-btn';
     btnCopy.style.width = '25px';
